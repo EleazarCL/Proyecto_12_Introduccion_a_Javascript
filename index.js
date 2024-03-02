3c347c21a33f2a95db000c1f297f3390a596ea5b
Eleazar Jara <eleazartmw15@gmail.com>
2024-03-02 14:14:31 -0300

    se sube tarea de javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+let precio = 400000
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+/*Captura de objetos*/
+let sumarBoton = document.getElementById("sumar");
+let restarBoton = document.getElementById("restar");
+let cantidadSpan = document.querySelector(".cantidad");
+let valor_totalSpan = document.querySelector(".valor-total");
+
+/*EDITAR Y TRABAJAR DESDE ACA ABAJO */
+
+cantidadSpan.innerHTML=0;
+
+/*const nuevaCantidad=Number(cantidadSpan.innerHTML)+1;
+cantidadSpan.innerHTML=nuevaCantidad;
+
+const nuevoPrecio = Number(precioSpan.innerHTML)*nuevaCantidad;
+valor_totalSpan.innerHTML=nuevoPrecio;
+ */
+
